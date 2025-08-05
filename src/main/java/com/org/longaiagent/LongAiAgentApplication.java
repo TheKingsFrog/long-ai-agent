e62eedc7d1c712d5e2477742cb78574965695b15
ethan <1625650213@qq.com>
2025-08-05 22:50:57 +0800

    initial commit
    1.第一天提交，随便提提，总比没进度好！

---
+++ src/main/java/com/org/longaiagent/LongAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.org.longaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongAiAgentApplication.class, args);
+    }
+
+}
