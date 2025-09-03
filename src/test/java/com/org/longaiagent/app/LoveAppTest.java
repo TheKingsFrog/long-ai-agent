3489a887b1112074f17aa6239bb4036b9a0108c3
ethan <1625650213@qq.com>
2025-09-04 00:31:55 +0800

    feat(250904):多轮对话 demo

---
+++ src/test/java/com/org/longaiagent/app/LoveAppTest.java
@@ -0,0 +1,38 @@
+package com.org.longaiagent.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Autowired
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+
+        String chatId = UUID.randomUUID().toString();
+
+        // 第一轮
+        String msg1 = "你好，我是洪成隆";
+        String answer = loveApp.doChat(msg1, chatId);
+
+        // 第二轮
+        String msg2 = "我想追寻爱情";
+        answer = loveApp.doChat(msg2, chatId);
+        Assertions.assertNotNull(answer);
+
+        // 第三轮
+        String msg3 = "快餐时代，能找到真爱吗?还记得我叫什么名字吗？";
+        answer = loveApp.doChat(msg3, chatId);
+        Assertions.assertNotNull(answer);
+
+    }
+}
